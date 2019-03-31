26eda5b4b3eab2d047ea48567787143ccbe118a7
bamsupitch <bamsupitchaya2540@gmail.com>
2019-03-31 10:37:50 +0700

    testController

--- controller/testController.js
+++ controller/testController.js
@@ -12,11 +12,30 @@ router.get("/getData",  (req, res) => {
     let result = req.query.key1 * req.query.key2;
     res.send({ result: result })
 });
+
+router.get("/getTriangle",  (req, res) => {
+    let result = 0.5 *req.query.base * req.query.high;
+    res.send({ Triangle: result })
+});
+
+router.get("/getBMI",  (req, res) => {
+    let result = Number(req .query.weight/((req.query.height/100)*(req.query.height/100))).toFixed(2);
+    let arg ="";
+    if (result>25){
+        arg= "Fat"
+    }else if (result==25){
+        arg ="Normal"
+    }else if (result < 25){
+        arg = "thin"
+    }
+    res.send({ BMI: result,status: arg})
+});
 /*
 Request
 * */
 router.post("/postData",async  (req, res) => {
-    if(!req.body.key5){
+    if(!req.body.key){
+        responseObject.result="";
         responseObject.msg = "Bad request";
     }else{
         responseObject.msg = "success";
